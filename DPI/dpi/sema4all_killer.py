6c627d5981288fb56a6bd5f0c2c34dd206df046b
pintamonas4575 <alejandro.embi@gmail.com>
2024-10-21 13:45:51 +0200

    checkpoint estrategia sema4all

--- DPI/dpi/sema4all_killer.py
+++ DPI/dpi/sema4all_killer.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from player import Player
 from dilemma import Dilemma, C, D
 
@@ -9,8 +10,23 @@ class Sema4All_Killer(Player):
         """Sema4All_Killer"""
         super().__init__(dilemma,name)
 
-        raise NotImplementedError
+        self.first_movements=[C,C]
+        self.opponent_defections = 0
 
     def strategy(self, opponent: Player) -> int:
-        """"""
-        raise NotImplementedError
+        """A veces le da un TIC en el que no se sabe qué es lo que sacará"""
+        self.iteration += 1
+
+        if self.iteration < 2: # la nº '0' y '1'
+            return self.first_movements[self.iteration]
+        
+        if self.iteration % 3 == 0:  # caso periódico de TIC
+            if random.random()+0.1 > 0.5:
+                return D
+        else:
+            return C
+        
+        # analizar últimos 3 movimientos del oponente
+        last_opponent_moves = opponent.history[:-3]
+        return max(set(last_opponent_moves), key=last_opponent_moves.count)
+
